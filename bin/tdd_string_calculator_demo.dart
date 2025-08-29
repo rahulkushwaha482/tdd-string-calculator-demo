964e63b8dcd326881296c40d86d44fb65ab5e4b2
Rahul Kushwaha <rahulkushwaha482@gmail.com>
2025-08-29 20:24:14 +0530

    feat: String Calculator class added with empty class file

---
+++ bin/tdd_string_calculator_demo.dart
@@ -0,0 +1,5 @@
+import 'package:tdd_string_calculator_demo/tdd_string_calculator_demo.dart' as tdd_string_calculator_demo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdd_string_calculator_demo.calculate()}!');
+}
---
+++ test/tdd_string_calculator_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdd_string_calculator_demo/tdd_string_calculator_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
